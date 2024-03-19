09a6c582a96fb4593b13ebcabe509acc2f4013a1
Muhammad Dzakwan <dzakwanattaqiy77@gmail,com>
2024-03-19 08:11:00 +0700

    Coba

---
+++ PERTEMUAN_3/kondisi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int nilai;
+    cout << "Masukkan nilai : "; cin >> nilai;
+    if (nilai <= 65){
+        cout << "Anda tidak lulus" << endl;
+    } 
+
+    if (nilai <= 65){
+        cout << "Anda tidak lulus" << endl;
+    }else {
+        cout << "anda lulus" <<endl;
+    }
+}
