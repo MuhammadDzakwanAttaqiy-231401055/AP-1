196f505417e20f0f3e89f53b0f81e245621f4d43
Muhammad Dzakwan <dzakwanattaqiy77@gmail,com>
2024-05-14 14:18:21 +0700

    Pertemuan 1: Setup Compiler & Input-Output & Variables

--- PERTEMUAN_1/main.c
+++ PERTEMUAN_1/main.c
@@ -1,23 +1,36 @@
+//* library untuk Bahasa C
 #include <stdio.h>
 
 void main(){
+    //* Deklarasi variabel
     int nim,a;
-    char kom[100],nama[100];
+    //* Pada bahasa C tidak ada string, jadi pakai char array
+    char kom,nama[100];
     float ip;
 
-    printf("Hello world\n");
+    //* Print digunakan untuk menampilkan output
+    printf("Hello world\n");  //* /n = newline
     printf("Masukkan Nama : ");
-    gets(nama);
+
+    //* gets digunakan untuk menerima inputan dan dapat lebih dari 1 kata
+    gets(nama); //* gets = Get String
     printf("Masukkan NIM : ");
+
+    //* scanf = input tanpa spasi, "%d" = tipe data variabelnya, &nim = variabel yg menerima input
     scanf("%d", &nim); 
     printf("Masukkan KOM : ");
-    scanf("%s", &kom); 
+    scanf("%c", &kom); 
     printf("Masukkan IP : ");
     scanf("%f", &ip); 
 
+    //* puts = menampilkan output dan menyertakan /n diakhirnya
     printf("Nama : "); puts(nama);
     printf("NIM : %d\n", nim);
     printf("KOM : %c\n", kom);
     printf("IP : %.3f\n", ip);
+
+    //* getch() = getcharacter, menerima input karakter
+    printf("Press any button to continue...");
+    getch();
 
 }
